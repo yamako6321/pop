7018f9ae932b9eb47ce7875e511d802c90d55b3e
yamako6321 <124650861+yamako6321@users.noreply.github.com>
2023-09-11 23:39:39 +0700

    pop

--- main.py
+++ main.py
@@ -1,1 +1,61 @@
-print('j')
+import re
+
+
+
+
+# word = 'АЯrуенКЕНГАПРОкенгпрогя'
+# for i in word:
+#     if ord(i)>=1040 and ord(i)<=1071:
+#         print(i)
+# count = 0
+# for i in range (len(text_1)):
+#     if text_1[i] in '.?!':
+#         shift = 1
+#         while i+shift<len(text_1)-1 and text_1[i+shift] == ' ':
+#             shift += 1
+#         if i+shift >= len(text_1)-1:
+#             break
+#         if ord(text_1[i+shift])>=1040 and ord(text_1[i+shift])<=1071:
+#             count+=1
+# print(count)
+
+text = input()
+# считает прелдложения
+count_sentence = 0
+for i in text:
+    if i in '.!?':
+        count_sentence += 1
+# print(count_sentence)
+
+# считает слоги
+count_vowels = 0
+for i in text:
+    if 'а' in i or 'о' in i or 'у' in i or 'ы' in i \
+            or 'э' in i or 'е' in i or 'ё' in i or 'и' in i \
+            or 'ю' in i or 'я' in i or 'А' in i or 'О' in i \
+            or 'У' in i or 'Ы' in i or 'Э' in i or 'Е' in i \
+            or 'Ё' in i or 'И' in i or 'Ю' in i or 'Я' in i:
+        count_vowels += 1
+# print(count_vowels)
+
+# как считать количество слов
+count_words = text.count(" ") + 1
+# print(count_words)
+
+# индекс Флеша
+I_F = 206.835 - 1.3 * (count_words/count_sentence) - 60.1 * (count_vowels/count_words)
+print(I_F)
+if I_F >= 90 and I_F <= 100:
+    print('Очень высокий - 5 классов')
+if I_F >= 80 and I_F < 90:
+    print('Высокий - 6 классов')
+if I_F >= 70 and I_F < 80:
+    print('Выше среднего - 7 классов')
+if I_F >= 60 and I_F < 70:
+    print('Средний - 8-9 классов')
+if I_F >= 50 and I_F < 60:
+    print('Ниже среднего - 10-11 классов')
+if I_F >= 30 and I_F < 50:
+    print('Низкий - вуз')
+if I_F >= 0 and I_F < 30:
+    print('Очень низкий - выпускник вуза')
